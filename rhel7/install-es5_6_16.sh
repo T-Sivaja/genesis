87facce6e9e9b8a12a7d2569ae798bbd3dff7db3
Sivaja Patamornchai <sivaja@convolab.ai>
2019-07-19 09:41:43 +0700

    Implement install Elasticsearch 5.6.16

--- rhel7/install-es5_6_16.sh
+++ rhel7/install-es5_6_16.sh
@@ -1,6 +1,7 @@
-cat /etc/redhat-release;
-uname -a;
-yum repolist all;
-cat /etc/os-release;
-hostname;
-ip a;
+cat /etc/redhat-release;echo "===================================================";cat /etc/os-release;echo "===================================================";hostname;echo "===================================================";yum repolist all;echo "===================================================";ip a;echo "===================================================";yum install docker && systemctl enable docker.service && systemctl start docker.service && docker run -d -p 9200:9200  -p 9300:9300 -e "http.host=0.0.0.0" -e "transport.host=127.0.0.1" -e "node.name=tot-chatbot01" --restart unless-stopped --name elasticsearch docker.elastic.co/elasticsearch/elasticsearch:5.6.16 && docker stats -a
+
+
+
+
+
+
